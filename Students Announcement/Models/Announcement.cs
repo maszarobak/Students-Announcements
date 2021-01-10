a1b5e65f86208c11f49a1514d55d6b8c795d2199
maszarobak <mariia.robaczewska@gmail.com>
2021-01-10 22:04:15 +0100

    regexy

--- Students Announcement/Models/Announcement.cs
+++ Students Announcement/Models/Announcement.cs
@@ -10,17 +10,29 @@ namespace Students_Announcement.Models
     {
         public int id { get; set; }
 
-       
+        [Display(Name = "Uczelnia")]
+        [RegularExpression (@"^[A-Z]*$", ErrorMessage = "Proszę wpisać nazwę uczelni w postaci np. UG.")]
         public string uczelnia { get; set; }
-       
+
+        [Display(Name = "Wydział")]
+        [Required(ErrorMessage = "Proszę wpisać nazwę wydziału w postaci np. WZR")]
         public string wydzial { get; set; }
-        
+
+        [Display(Name = "Autor")]
+        [Required(ErrorMessage = "Proszę podac imię, nazwisko lub nick")]
         public string autor { get; set; }
-       
+
+        [Display(Name = "Tytuł")]
+        [Required(ErrorMessage = "Proszę podać tytuł")]
         public string tytul { get; set; }
-       
+
+        [Display(Name = "Kategornia")]
+        [Required(ErrorMessage = "Proszę wybrać kategorię")]
         public string kategoria { get; set; }
-       
+
+        [Display(Name = "Ogłoszenie")]
+        [StringLength(200, MinimumLength = 3)]
+        [Required(ErrorMessage = "Proszę wpisać ogłoszenie")]
         public string opis { get; set; }
     }
 }
