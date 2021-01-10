7fffbf099ef6940e4211c32a1b1489e20024fa90
maszarobak <mariia.robaczewska@gmail.com>
2021-01-10 19:23:49 +0100

    regularexpression

--- Students Announcement/Models/Announcement.cs
+++ Students Announcement/Models/Announcement.cs
@@ -10,17 +10,17 @@ namespace Students_Announcement.Models
     {
         public int id { get; set; }
 
-        [Required(ErrorMessage = "Proszę wpisać uczelnie.")]
+        [RegularExpression(@"^[A-Za-z0-9]*$", ErrorMessage = "Proszę wpisać uczelnie.")]
         public string uczelnia { get; set; }
-        [Required(ErrorMessage = "Proszę wpisać wydział")]
+        [RegularExpression(@"^[A-Za-z0-9]*$", ErrorMessage = "Proszę wpisać wydział")]
         public string wydzial { get; set; }
-        [Required(ErrorMessage = "Proszę podac imię, nazwisko lub nick")]
+        [RegularExpression(@"^[A-Za-z0-9]*$", ErrorMessage = "Proszę podac imię, nazwisko lub nick")]
         public string autor { get; set; }
-        [Required(ErrorMessage = "Proszę podać tytuł")]
+        [RegularExpression(@"^[A-Za-z0-9]*$", ErrorMessage = "Proszę podać tytuł")]
         public string tytul { get; set; }
-        [Required(ErrorMessage = "Proszę wybrać kategorię")]
+        [RegularExpression(@"^[A-Za-z0-9]*$", ErrorMessage = "Proszę wybrać kategorię")]
         public string kategoria { get; set; }
-        [Required(ErrorMessage = "Proszę wpisać ogłoszenie")]
+        [RegularExpression(@"^[A-Za-z0-9]*$", ErrorMessage = "Proszę wpisać ogłoszenie")]
         public string opis { get; set; }
     }
 }
